38cf0b761364ca4cb756a7634470ef562a6b5da3
Kuchumo35 <03230197.gcbs@rub.edu.bt>
2024-03-01 12:56:39 +0600

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,31 @@
+#Objective: a calculator application
+# variables and if statement
+
+#steps
+#1. get input from the user
+#2. do calculation based on user input
+#3. give output to the user
+
+print('*')
+print('+')
+print('-')
+print('/')
+
+whatusertyped = input()
+
+print('user typed:', whatusertyped)
+print('user input-type', type(whatusertyped))
+
+print('-------------')
+if whatusertyped == "+":
+    print('Doing Addition')
+
+print('doing more addition')
+
+if whatusertyped == "-":
+    print('doing subtraction')
+    print ('doing more subtraction')
+
+if whatusertyped == "*":
+    print('doing multiplication')
+    print('doing more multiplication')
---
+++ datatype.py
@@ -0,0 +1,8 @@
+variable1 = 10
+name ='luffy'
+name2 = 'zoro'
+score = 10.1 
+didhewin = True
+print(variable1)
+print(type(variable1))
+
