be6cca4eceab131cf3a214886f9aeae071a0bc19
Kuchumo35 <03230197.gcbs@rub.edu.bt>
2024-03-07 15:04:50 +0600

    interpolation

--- strings.py
+++ strings.py
@@ -5,3 +5,11 @@ combined1 = "hi" + " " + "there"
 print ('string concatenation')
 print (combined)
 print (combined1)
+
+# ! string interpolation
+print('String Interpolation')
+# ! how do you print "john is 30 years of age"
+name = "John"
+age = 99
+interpolated = f"John is {age} years of age"
+print(interpolated)
