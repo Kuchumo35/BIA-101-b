5e9f36474f1c626f4943b38e78c3fe25f36d05a8
Kuchumo35 <03230197.gcbs@rub.edu.bt>
2024-03-14 15:25:03 +0600

    functions

--- functions.py
+++ functions.py
@@ -1,9 +1,15 @@
 def greet():
-    # do some calculation code
-    # complex stuff
-    print('Hello there')
+    # do some calculation code 
+    # complex stuffs
+    print('Hello')
 
-def user_input = 'Nima'
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    print('adding numbers')
+    return sum
 
-if user_input == "Nima":
-    greet_Nima()
+userNumber = 10
+userNumber2 = 20
+
+returnedSum = add_numbers(userNumber, userNumber2)
+print(returnedSum)
